39c820c2e316363b90b94f3feda1b65a5cb602f8
Dawid <77678316+HackZy01@users.noreply.github.com>
2026-08-04 00:52:00 +0200

    Localising new strings + minor fix

--- mods/ui/speedUI.js
+++ mods/ui/speedUI.js
@@ -1,5 +1,6 @@
 import { configRead } from '../config.js';
 import { showModal, buttonItem, overlayPanelItemListRenderer } from './ytUI.js';
+import { t } from 'i18next';
 
 const interval = setInterval(() => {
     const videoElement = document.querySelector('video');
@@ -110,7 +111,7 @@ function speedSettings() {
         )
     );
 
-    showModal('Playback Speed', overlayPanelItemListRenderer(buttons, selectedIndex), 'tt-speed');
+    showModal(t('player.playbackSpeed.title'), overlayPanelItemListRenderer(buttons, selectedIndex), 'tt-speed');
 }
 
 export {
